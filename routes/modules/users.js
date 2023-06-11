b464b4e2cf57c4e37013ab29101c5ee1b47525b6
LAI-Recycle <xfjioyhnplus@gmail.com>
2023-06-11 21:40:37 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,6 +5,15 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
+router.post('/login', (req, res) => {
+ 
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+
 module.exports = router
 
 
