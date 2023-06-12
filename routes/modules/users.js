1052593650a22285ae3f49056467c8553422416a
LAI-Recycle <xfjioyhnplus@gmail.com>
2023-06-12 22:46:24 +0800

    feat: add isAuthenticated to display different view templates

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -45,7 +45,7 @@ router.post('/register', (req, res) => {
   .catch(err => console.log(err))
 })
 
-router.get('logout', (req, res)=> {
+router.get('/logout', (req, res) => {
   req.logout()
   res.redirect('/users/login')
 })
