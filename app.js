a92252fd42a9178593b5b81c8307713c2e63bbca
LAI-Recycle <xfjioyhnplus@gmail.com>
2023-06-12 22:15:01 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
+// 載入設定檔，要寫在 express-session 以後
+const usePassport = require('./config/passport')
+
 const routes = require('./routes')
 require('./config/mongoose')
+
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -25,6 +29,9 @@ app.use(session({
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+usePassport(app)
 
 app.use(routes)
 
